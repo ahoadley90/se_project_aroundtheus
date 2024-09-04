5e86ab6baa5dca84bf4d76ec229cecc7d1b6cbbc
AmandaKim <akimmets@gmail.com>
2024-09-03 21:58:33 -0500

    updated open and close modal

--- scripts/index.js
+++ scripts/index.js
@@ -26,9 +26,24 @@ const initialCards = [
 ];
 
 const profileEditButton = document.querySelector("#profile__edit-button");
-const profileEditModal = document.querySelector("#profile__edit-modal");
+const profileEditModal = document.querySelector("#profile__edit_modal");
+const profileEditCloseButton = profileEditModal.querySelector(".modal__close");
 const modalCloseButton = document.querySelector(".modal__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile__title_input");
+const profileDescriptionInput = document.querySelector(
+  "#profile__description_input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+
+function closePopop() {
+  profileEditModal.classList.remove("modal__opened");
+}
+
 profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   if (profileEditModal.classList.contains("modal__opened")) {
     profileEditModal.classList.remove("modal__opened");
   } else {
@@ -44,9 +59,29 @@ modalCloseButton.addEventListener("click", () => {
   profileEditModal.style.display = "none";
 });
 
+function closePopup() {
+  profileEditModal.classList.remove("modal__opened");
+}
+
 window.addEventListener("click", (event) => {
   if (event.target === profileEditModal) {
     profileEditModal.style.display = "none";
     console.log("Modal closed by clicking outside");
   }
+});
+
+profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+  profileEditModal.classList.add("modal__opened");
+});
+
+profileEditCloseButton.addEventListener("submit", () => {
+  closePopop();
+});
+
+profileEditForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
 });
