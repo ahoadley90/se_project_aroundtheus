ddded0b027fc8b8e95b27582147203e83ee9b880
AmandaKim <akimmets@gmail.com>
2024-11-05 16:30:25 -0600

    update with Popup class and PopupWithForm class

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,37 @@
+import Popup from "./Popup.js";
+
+export default class PopupWithForm extends Popup {
+  constructor(popupSelector, handleFormSubmit) {
+    super(popupSelector);
+    this._handleFormSubmit = handleFormSubmit;
+    this._form = this._popup.querySelector(".modal__form");
+    this._inputs = this._form.querySelectorAll(".modal__input");
+  }
+
+  _getInputValues() {
+    const inputValues = {};
+    this._inputs.forEach((input) => {
+      inputValues[input.name] = input.value;
+    });
+    return inputValues;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+    });
+  }
+
+  close() {
+    super.close();
+    this._form.reset();
+  }
+
+  setInputValues(data) {
+    this._inputs.forEach((input) => {
+      input.value = data[input.name];
+    });
+  }
+}
